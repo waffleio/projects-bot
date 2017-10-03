0870247a54a473696042f41b7e43a628a4b8e8f5
Andrew Homeyer <homeyer@gmail.com>
2017-10-02 20:55:57 -0600

    again!

--- marketplace-routes.js
+++ marketplace-routes.js
@@ -1,10 +1,10 @@
+const bodyParser = require('body-parser');
 
 module.exports = (app) => {
+  app.use(bodyParser.json());
 
   app.post('/marketplace', (req, res, next) => {
-
     console.log(`Thanks for purchasing the ProjectsBot ${req.body.marketplace_purchase.plan.name} plan!`);
-
   });
 
 }
